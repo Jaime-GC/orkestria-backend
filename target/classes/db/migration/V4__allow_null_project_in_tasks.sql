ca2fc2f460e60ce3c7ad0eddb2f9e3b978e4e5e9
Jaime-GC <jgcarbajo1@gmail.com>
2025-07-06 19:06:16 +0200

    Remove target/ directory from repository and update .gitignore

--- target/classes/db/migration/V4__allow_null_project_in_tasks.sql
+++
@@ -1,2 +0,0 @@
--- Liquibase Migration Script to allow NULL project_id in tasks table
-ALTER TABLE tasks ALTER COLUMN project_id DROP NOT NULL;
--- target/classes/db/migration/V5__add_title_to_employee_schedules.sql
+++
@@ -1,8 +0,0 @@
--- First add the column as nullable
-ALTER TABLE employee_schedules ADD COLUMN IF NOT EXISTS title VARCHAR(255);
-
--- Then update existing records with a default title
-UPDATE employee_schedules SET title = 'Schedule - ' || username WHERE title IS NULL;
-
--- Finally, set the column to NOT NULL after all records have a value
-ALTER TABLE employee_schedules ALTER COLUMN title SET NOT NULL;
